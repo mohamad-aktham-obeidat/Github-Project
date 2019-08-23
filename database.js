b050e9451ebdcb882819fa17c2feb56075e87fc2
mohamadAkthamObeidat <mohamadaktham7@gmail.com>
2019-08-23 12:02:00 +0300

    First Commit

--- database.js
+++ database.js
@@ -1,16 +1,18 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/Repos', { useNewUrlParser: true });
+// Import Mongoose Files
+const mongoose = require('mongoose'); 
+// Connect This File To My DataBase.
+mongoose.connect('mongodb://localhost/GitHubRepos', { useNewUrlParser: true });
+// Store 'mongoose.connection' in Variable Called "db".
 const db = mongoose.connection;
-
+// Show Error Message If Connection Not Success
 db.on('error', function () {
   console.log('mongoose connection error');
-  console.log('____________________________')
+  console.log('__________________________________________')
 });
-
+// Show Success Message If Connection Successful
 db.once('open', function () {
   console.log('mongoose connected successfully');
-  console.log('____________________________')
+  console.log('__________________________________________')
 });
 
 /*
@@ -43,4 +45,44 @@ module.exports = {
 
 // Start your code below
 
+// Create New Mongo Scheme To Store Our Data In It.
+let repositoryScheme = new mongoose.Schema({
+  title: String,
+  language: String,
+  status: Boolean
+})
+
+// Store The Scheme In Variable To Deal With This Variable.
+let Repositories = mongoose.model('repositories', repositoryScheme);
+
+// @METHOD 'getRepositories'
+// Return All Repositories From Database
+let getRepositories = (callBack) => {
+  console.log(callBack);
+  Repositories.find({}, (error, response) => {
+    if (error) {
+      callBack(error);
+    } else {
+      callBack(response);
+    }
+  })
+}
+
+// @METHOD
+// Add Specific Repository To Database.
+let addNewRepository = (newRepository, callBack) => {
+  Repositories.create(newRepository, (error, response) => {
+    if(error){
+      callBack(error)
+    } else {
+      getRepositories(callBack)
+    }
+  })
+} 
+
+
+module.exports = {
+  getRepositories,
+  addNewRepository
+}
 
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require('body-parser')
 const mongo = require("./database");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.json('server is working');
@@ -20,10 +24,46 @@ app.get('/tasks', (req, res) => {
 */
 
 // Start your code below
+// @GET
+// To Check If Server Works Or Not
+app.get('/', (req, res) => {
+  res.json('Server Works *.* ')
+  
+})
 
+// @GET
+// Return All Repositories From Database
+app.get('/repositories', (req, res) => {
+  mongo.getRepositories(result => {
 
+    res.json(result)
+  })
+})
 
+// @POST
+// Add Specific Repository To Database.
+app.post('/repositories', (req, res) => {
+  mongo.addNewRepository(req.body, (response)=>{
+    
+    res.json(response);
+  })
+})
 
+// @PUT
+// Update Repository Status From (Public => Private) && (Private => Public).
+app.put('/repositories/:id', (req, res) => {
+
+  console.log(res);
+  res.json(res);
+})
+
+// @DELETE
+// Delete Specific Repository To Database.
+app.delete('/repositories/:id', (req, res) => {
+
+  console.log(res);
+  res.json(res);
+})
 
 
 
